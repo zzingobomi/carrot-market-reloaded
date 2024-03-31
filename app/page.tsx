b53f2c89773a270dda1cfa939563c21c11917cd4
zzingobomi <zzingo5@gmail.com>
2024-03-31 10:30:46 +0900

    tailwind 3.4

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-5">
+        <input
+          className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent 
+                    focus:ring-orange-500 focus:ring-offset-2 transition-shadow
+                    placeholder:drop-shadow"
+          type="text"
+          placeholder="Search here..."
+        />
+        <button
+          className="bg-black text-white py-2 rounded-full 
+        active:scale-90 focus:scale-90 transition-transform font-medium outline-none"
+        >
+          Search
+        </button>
+      </div>
+    </main>
+  );
 }
