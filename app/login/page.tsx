b452f9bfc6461eed9c703684eeaa31db474f474e
zzingobomi <zzingo5@gmail.com>
2024-04-04 20:22:41 +0900

    #5.2 useFormStatus

---
+++ app/login/page.tsx
@@ -0,0 +1,37 @@
+import FormButton from "@/components/form-btn";
+import FormInput from "@/components/form-input";
+import SocialLogin from "@/components/social-login";
+
+export default function LogIn() {
+  async function handleForm(formData: FormData) {
+    "use server";
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("logged in!");
+  }
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요!</h1>
+        <h2 className="text-xl">Log in with email and password.</h2>
+      </div>
+      <form action={handleForm} className="flex flex-col gap-3">
+        <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          errors={[]}
+        />
+        <FormButton text="Log in" />
+      </form>
+      <SocialLogin />
+    </div>
+  );
+}
---
+++ components/form-btn.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+interface FormButtonProps {
+  text: string;
+}
+
+export default function FormButton({ text }: FormButtonProps) {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      disabled={pending}
+      className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
+    >
+      {pending ? "로딩 중" : text}
+    </button>
+  );
+}
